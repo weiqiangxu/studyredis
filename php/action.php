5283479daf90647ec24a6fb295af8ea7c3e962a2
435861851@qq.com <435861851@qq.com>
2018-06-05 23:01:35 +0800

    add

--- php/action.php
+++ php/action.php
@@ -3,11 +3,37 @@
 // 文章列表
 $article = new article();
 
-// 添加文章
-$data = ['title'=>'东方不败','content'=>'打不过的时候想想，同学你充钱了吗？','author'=>'马化腾','time'=>time()];
+// 添加文章-序列化
+$data = [   
+        'title'=>'东方不败',
+        'content'=>'打不过的时候想想，同学你充钱了吗？',
+        'author'=>'马化腾',
+        'time'=>time()
+    ];
 $res = $article->addArticle($data);
 
 
-// 读取文章
+// 读取文章-字符串
 $res = $article->getArticle(16);
-var_dump($res);die;
+
+// 添加文章-散列
+$data = [   
+        'title'=>'东方不败',
+        'content'=>'打不过的时候想想，同学你充钱了吗？',
+        'author'=>'马化腾',
+        'time'=>time()
+    ];
+$res = $article->addArt($data);
+
+// 读取文章
+$res = $article->getArt(52,'title');
+
+// 编辑文章
+$res = $article->setArt(38,['title'=>'大王叫我来巡山']);
+
+// 删除文章
+$res = $article->delArt(38);
+var_dump($res);
+// 读取文章
+$res = $article->getArt(58);
+var_dump($res);
--- php/model.php
+++ php/model.php
@@ -24,7 +24,7 @@ class article
 
 
     /**
-        * 添加文章
+        * 添加文章 - 字符串
         * @param  [arr] $data
         * @author xu
         * @copyright 2018-06-04
@@ -38,17 +38,12 @@ class article
         $serializePost = serialize($data);
 
         $res = $client->set('post:'.$postId.':data', $serializePost);
-        
-        if($res = 'OK')
-        {
-            $data = '添加成功！';
+        if($res = 'OK'){
+            $data = ['status'=>true,'data'=>'添加成功！'];
+        }else{
+            $data = ['status'=>false,'data'=>'post:'.$postId.':data'.' 已经存在！'];
         }
-        else
-        {
-            $data = 'post:'.$postId.':data'.' 已经存在！';
-        }
-
-        return ['status'=>true,'data'=>$data];
+        return $data;
     }
 
 
@@ -64,12 +59,72 @@ class article
         $res = $client->get('post:'.$id.':data');
         
         if(empty($res)) {
-            $data = '文章 post:'.$id.':data 不存在！';
+            $data = ['status'=>false,'data'=>'文章 post:'.$id.':data 不存在！'];
         }else{
             // 序列化文章存储
-            $data = unserialize($res); 
+            $data = ['status'=>true,'data'=>unserialize($res)]; 
+        }
+        return $data;
+    }
+
+    /**
+        * 添加文章 - 散列 - 解决字符串序列存储导致的内存浪费/单一字段修改的竞态问题
+        * @param  [arr] $data
+        * @author xu
+        * @copyright 2018-06-04
+    */
+    function addArt($data)
+    {
+        $client = $this->client;
+
+        // 获取自增ID
+        $postId = $client->incr('post:count'); 
+        // foreach ($data as $k => $v){
+        //     $client->hset('post:'.$postId.':data',$k,$v);
+        // }
+        echo $postId;
+        $client->hmset('post:'.$postId.':data',$data);
+        return ['status'=>true,'data'=>'success'];
+    }
+
+    /**
+     * 读取文章 -散列
+    */
+    function getArt($id,$field='all')
+    {
+        $client = $this->client;
+
+        if($field=='all') {
+            $data = $client->hgetall('post:'.$id.':data');
+        }else{
+            $data = $client->hget('post:'.$id.':data',$field);
         }
         return ['status'=>true,'data'=>$data];
     }
+
+    /**
+     * 编辑文章 -散列
+    */
+    function setArt($id,$data)
+    {
+        $client = $this->client;
+        foreach ($data as $k => $v) {
+            $client->hset('post:'.$id.':data',$k,$v);
+        }
+        return ['status'=>true,'data'=>true];
+    }
+
+
+    /**
+     * 删除文章 -散列
+    */
+    function delArt($id)
+    {
+        $client = $this->client;
+        $data = ['title','content','author','time'];
+        $client->hdel('post:'.$id.':data',$data);
+        return ['status'=>true,'data'=>true];
+    }
+
 
 }
